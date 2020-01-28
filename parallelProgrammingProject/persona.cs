3118b63bf1f745f89ffd0f8556f29304546f25a4
GuillemExe <guillem.arrojo@mataro.epiaedu.cat>
2020-01-28 20:51:09 +0100

    refactorizado

--- parallelProgrammingProject/persona.cs
+++ parallelProgrammingProject/persona.cs
@@ -15,23 +15,24 @@ namespace parallelProgrammingProject
 
         public bool comprova_dni()
         {
-            if (dni.Length == 9)
-            {
-                var stringNumericDni = dni.Substring(0, 8);
-                var stringLetterDni = dni.Substring(8, 1);
+            // THE DNI MUST HAVE THE 9 DIGITS
+            if (dni.Length != 9) return false;
+            // PARSE VALUE SUBSTRING NUMERIC
+            var stringNumericDni = dni.Substring(0, 8);
 
-                var stringLetterDniReturn = CheckLetterDni(stringNumericDni);
+            // PARSE VALUE SUBSTRING LETTER
+            var stringLetterDni = dni.Substring(8, 1);
 
-                return stringLetterDni.Equals(stringLetterDniReturn);
-            }
-            else
-            {
-                return false;
-            }
+            // CALCULATE LETTER THROUGH THE NUMBER VALUE
+            var stringLetterDniReturn = CheckLetterDni(stringNumericDni);
+
+            // COMPARISON OF THE CALCULATED LETTER WITH THE LETTER INTRODUCED
+            return stringLetterDni.Equals(stringLetterDniReturn);
         }
 
         public string CheckLetterDni(string valorNumericDni)
         {
+            // MAP LETTER DNI
             string[] letterMap =
             {
                 "T", "R", "W", "A", "G", "M",
@@ -40,10 +41,13 @@ namespace parallelProgrammingProject
                 "H", "L", "C", "K", "E"
             };
 
+            // PARSE VALUE NUMERIC EXAMPLE STRING "12345678" ON INTEGER
             var numericDni = int.Parse(valorNumericDni);
 
+            // WE MAKE THE CALCULATION AND WILL GIVE US THE POSITION OF THE LETTER
             var aux = numericDni % 23;
 
+            // AND RETURN LETTER
             return letterMap[aux];
         }
 
@@ -54,8 +58,10 @@ namespace parallelProgrammingProject
 
         public bool comprova_nom()
         {
+            // SPLIT STRING NAME AND SURNAMES
             var splitName = nombre.Split(null);
 
+            // THERE MUST BE MORE THAN ONE NAME AND MORE THAN TWO LETTERS
             return splitName.Length > 1 && splitName.All(t => t.Length >= 3);
         }
     }
