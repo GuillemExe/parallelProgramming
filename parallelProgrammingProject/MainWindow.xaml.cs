9dc85686ebe76c94d892dd141a542d33c3c2fd22
GuillemExe <guillem.arrojo@mataro.epiaedu.cat>
2020-01-28 18:34:26 +0100

    Lo primero

---
+++ parallelProgrammingProject/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace parallelProgrammingProject
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private string _dni;
+        private string _nombre;
+        private string _mail;
+
+        persona usuario = new persona();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonValidarDni_Click(object sender, RoutedEventArgs e)
+        {
+            var usuarioAux = usuario;
+
+            if (usuarioAux.comprova_dni() && usuarioAux.comprova_nom() && usuarioAux.comprova_mail())
+            {
+                char value = usuarioAux.calcula_lletra("ejemplo");
+            }
+        }
+
+        private void TextBoxDni_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            _dni = TextBoxDni.Text;
+            usuario.dni = _dni;
+        }
+
+        private void TextBoxNombre_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            _nombre = TextBoxNombre.Text;
+            usuario.nombre = _nombre;
+        }
+
+        private void TextBoxMail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            _mail = TextBoxMail.Text;
+            usuario.mail = _mail;
+        }
+    }
+}
---
+++ parallelProgrammingProject/persona.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parallelProgrammingProject
+{
+    class persona
+    {
+        public string dni { get; set; }
+        public string nombre { get; set; }
+        public string mail { get; set; }
+
+        public bool comprova_dni()
+        {
+            string stringNumeroDni;
+            string stringLetraDni;
+
+            char[] charLetraDni;
+
+            if (dni.Length == 9)
+            {
+                stringNumeroDni = dni.Substring(0, 8);
+                stringLetraDni = dni.Substring(8, 1);
+
+                charLetraDni = stringLetraDni.ToCharArray();
+                char letter = charLetraDni[0];
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool comprova_mail()
+        {
+            return false;
+        }
+
+        public bool comprova_nom()
+        {
+            return false;
+        }
+
+        public char calcula_lletra(string pasador)
+        {
+            return 'x';
+        }
+
+
+    }
+}
